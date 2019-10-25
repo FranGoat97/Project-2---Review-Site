3e33baefb6377534a3504f48aa97e9baca7355a0
Orrin Johnson <orrinjohnson@Orrins-MacBook-Air.local>
2019-10-25 15:02:15 -0600

    created a user post route

--- controllers/users.js
+++ controllers/users.js
@@ -18,6 +18,16 @@ router.get('/new', (req, res) => {
     res.render('users/new.ejs')
 });
 
+router.get('/', (req, res) => {
+    User.create(req.body, (err, createdUser) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.redirect('/users');
+        }
+    })
+})
+
 
 
 

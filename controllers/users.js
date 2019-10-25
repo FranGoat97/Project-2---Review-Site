fe11f5be94ff5dcf4d381b7a4d6601b437f7e1f7
Orrin Johnson <orrinjohnson@Orrins-MacBook-Air.local>
2019-10-25 13:13:23 -0600

    user index route and index.ejs files are linked

--- controllers/users.js
+++ controllers/users.js
@@ -2,4 +2,40 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
 
+router.get('/', (req, res) => {
+    User.find({}, (err, allUsers) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.render('users/index.ejs', {
+                users: allUsers
+            });
+        }
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;

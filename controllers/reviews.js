d74ce84463ddfc1c1b2b9a538c99504530761152
Orrin Johnson <orrinjohnson@Orrins-MacBook-Air.local>
2019-10-25 12:20:03 -0600

    shell codes are set up and both servers are running

---
+++ controllers/reviews.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const Review = require('../models/review.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const session = require('express-session');
 require('./db/db');
-const userController = require('./controllers/users');
-const reviewController = require('./controllers/reviews');
+app.use(session({
+    secret: "secret string",
+    resave: false, 
+    saveUninitialized: false 
+  }));
+const userController = require('./controllers/users.js');
+const reviewController = require('./controllers/reviews.js');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 app.use('/users', userController);

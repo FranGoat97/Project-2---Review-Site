29c46c98aed22b853c1c5b88bc881e42c3b3218c
Josh Vinlove <jjvinlove@gmail.com>
2019-10-25 15:01:55 -0600

    auth

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user.js');
+const bcrypt = require('bcryptjs');
 
 router.get('/', (req, res) => {
     User.find({}, (err, allUsers) => {
@@ -17,6 +18,31 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
     res.render('users/new.ejs')
 });
+
+// password 
+
+router.post('/new', (req, res, next) => {
+
+  // first we are going to hash the password
+  const password = req.body.password;
+  const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+  // lets create a object for our db entry;
+  const userDbEntry = {};
+  userDbEntry.username = req.body.username;
+  userDbEntry.password = passwordHash
+
+  // lets put the password into the database
+  User.create(userDbEntry, (err, user) => {
+    console.log(user)
+
+    // lets set up the session in here we can use the same code we created in the login
+    req.session.username = user.username;
+    req.session.logged   = true;
+    res.redirect('/users')
+  });
+
+})
 
 
 
